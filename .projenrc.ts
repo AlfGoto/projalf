3740983093f8e785eee31d1ac5f34e7ba83563aa
Alf <alfgoto@gmail.com>
2025-08-21 15:03:46 +0200

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,12 @@
-import { typescript } from 'projen';
-const project = new typescript.TypeScriptProject({
+import { cdk } from 'projen';
+const project = new cdk.JsiiProject({
+  author: 'Alf',
+  authorAddress: 'alfgoto@gmail.com',
   defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.8.0',
   name: 'projalf',
   projenrcTs: true,
+  repositoryUrl: 'git@github.com:AlfGoto/projalf.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
